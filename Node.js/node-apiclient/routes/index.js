43c2b3b695a5e3b8798b183608228c9d38d10567
kkkwp <chxrryda@gmail.com>
2022-12-02 11:46:47 +0900

    feat(api_client): token 발급 실습

---
+++ Node.js/node-apiclient/routes/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const axios = require('axios');
+
+// 매번 동일한 요청을 위한 URL을 상수로 설정
+const URL = 'http://localhost:8000/v1';
+
+// ajax 요청을 할 때 누가 요청했는지 확인해주기 위해서 origin header 추가
+axios.defaults.headers.origin = 'http://localhost:4000';
+
+// 토큰 발급 코드
+const request = async (req, api) => {
+    try {
+        if (!req.session.jwt) { // 세션에 토큰이 없으면
+            const tokenResult = await axios.post(`${URL}/token`, {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+            // res가 없어서 응답을 못 한다 -> 에러 처리 필요 없음
+            req.session.jwt = tokenResult.data.token;
+        }
+        // 토큰 내용 확인
+        const result = await axios.get(`${URL}${api}`, {
+            headers: { authorization: req.session.jwt }
+        });
+        return result;
+    } catch (error) {
+        // 토큰 유효 기간 만료
+        if (error.response.status === 419) {
+            // 기존 토큰 삭제
+            delete req.session.jwt;
+            // 다시 토큰을 생성해달라고 요청
+            return request(req, api);
+        }
+        return error.response;
+    }
+};
+
+const router = express.Router();
+
+router.get('/mypost', async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/test', async (req, res, next) => {
+    try {
+        if (!req.session.jwt) {
+            const tokenResult = await axios.post('http://localhost:8000/v1/token', {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+            if (tokenResult.data && tokenResult.data.code === 200) {
+                req.session.jwt = tokenResult.data.token;
+            } else {
+                // 토큰 발급 실패
+                return res.json(tokenResult.data);
+            }
+        }
+        // 토큰 내용 확인
+        const result = await axios.get('http://localhost:8000/v1/test', {
+            headers: { authorization: req.session.jwt }
+        });
+        return res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+module.exports = router;
---
+++ Node.js/node-apiserver/routes/v1.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const { verifyToken } = require('./middlewares');
+
+const jwt = require('jsonwebtoken');
+const { Domain, User, Post, Hashtag } = require('../models');
+
+const router = express.Router();
+
+// 데이터를 리턴하는 요청 처리
+router.get('/posts/my', verifyToken, (req, res) => {
+    Post.findAll({ where: { userId: req.decoded.id } })
+        .then((posts) => {
+            console.log(posts);
+            res.json({
+                code: 200,
+                payload: posts,
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json({
+                code: 500,
+                message: '서버 에러',
+            });
+        });
+});
+
+// 토큰 발급
+router.post('/token', async (req, res) => {
+    const { clientSecret } = req.body;
+    try {
+        // 도메인 찾아오기
+        const domain = await Domain.findOne({
+            where: { clientSecret },
+            include: {
+                model: User,
+                attribute: ['nick', 'id'],
+            }
+        });
+        if (!domain) {
+            return res.status(401).json({
+                code: 401,
+                message: '등록되지 않은 도메인입니다.'
+            });
+        }
+        // 토큰 생성
+        const token = jwt.sign({
+            id: domain.User.id,
+            nick: domain.User.nick,
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1m',    // 유효기간
+            issuer: 'nodebird'  // 발급자
+        });
+
+        return res.json({
+            code: 200,
+            message: '토큰이 발급되었습니다',
+            token
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러'
+        });
+    }
+});
+
+// 토큰을 확인하기 위한 처리
+router.get('/test', verifyToken, (req, res) => {
+    res.json(req.decoded);
+});
+
+module.exports = router;
