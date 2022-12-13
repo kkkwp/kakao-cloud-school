4fece696ef8ae405716c2b9cc17a705b2f07e9c9
kkkwp <chxrryda@gmail.com>
2022-12-13 14:50:55 +0900

    refact(useState): 함수형 컴포넌트에서 state 실습

--- React/react-component/src/StateComponent.jsx
+++ React/react-component/src/StateComponent.jsx
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class StateComponent extends Component {
-    /*
-    constructor(props) {
-        // 상위 클래스의 생성자 호출
-        super(props);
-        // state 생성
-        this.state = { number: 0 }
-    }
-    */
-    state = { number: 0 };
+const StateComponent = () => {
+    // 함수형 컴포넌트에서 state를 생성하는 방법
+    const [message, setMessage] = useState('');
 
-    render() {
-        return (<>
-            <p>숫자:{this.state.number}</p>
-            <button onClick={(e) => {
-                this.setState({ number: this.state.number + 1 }, () => {
-                    console.log('state의 값이 변경됨');
-                    console.log(this.state);
-                })
-            }}>증가</button>
-        </>)
-    }
+    // 버튼에 연결한 이벤트 처리 함수
+    const onClickEnter = (e) => { setMessage('안녕하세요') }
+    const onClickLeave = (e) => { setMessage('안녕히 가세요') }
+
+    const [color, setColor] = useState('black');
+
+    return (
+        <>
+            <button onClick={onClickEnter}>입장</button>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1 style={{ color }}>{message}</h1>
+            <button
+                style={{ color: 'red' }}
+                onClick={() => setColor('red')}>빨강</button>
+            <button
+                style={{ color: 'green' }}
+                onClick={() => setColor('green')}>녹색</button>
+            <button
+                style={{ color: 'blue' }}
+                onClick={() => setColor('blue')}>파랑</button>
+        </>
+    )
 };
 
 export default StateComponent;
