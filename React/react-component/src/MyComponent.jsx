8db1f105854eabab47840abaac06807db244fa38
kkkwp <chxrryda@gmail.com>
2022-12-13 14:27:18 +0900

    feat(component): class형 컴포넌트로 수정

--- React/react-component/src/MyComponent.jsx
+++ React/react-component/src/MyComponent.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+//클래스형 컴포넌트
+import React, { Component } from "react";
 
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ children, name, year }) => {
-    return (<>
-        <div>나의 컴포넌트의 이름은 {name} 입니다.</div>
-        <p>{children}</p>
-        <p>나는 {year}년에 태어났습니다.</p>
-    </>)
-};
+class MyComponent extends Component {
+    render() {
+        // props 가져오기
+        // 클래스형 컴포넌트에는 props 속성이 존재
+        const { children, name, year } = this.props;
+        return (<>
+            <div>나의 컴포넌트의 이름은 {name} 입니다.</div>
+            <p>{children}</p>
+            <p>나는 {year}년에 태어났습니다.</p>
+        </>);
+    }
+}
 
 MyComponent.propTypes = {
     name: PropTypes.string,
