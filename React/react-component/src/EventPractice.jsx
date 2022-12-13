26dc03f690c12945acdb647db9fe2dbe98816830
kkkwp <chxrryda@gmail.com>
2022-12-13 15:27:18 +0900

    feat(eventPractice): event routing 실습

--- React/react-component/src/EventPractice.jsx
+++ React/react-component/src/EventPractice.jsx
@@ -2,23 +2,28 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
     state = {
-        name: ''
+        name: '',
+        message: ''
     }
 
     //babel 이 인스턴스의 메서드로 변환을 자동으로 수행
     //this.handleChange로 이 메서드를 사용하는 것이 가능
     handleChange = (e) => {
+        //이벤트가 발생한 객체는 e.target
+        //e.target.name 은 이벤트가 발생한 객체의 name
         this.setState({
-            name: e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
     handleClick = (e) => {
-        alert(this.state.name);
+        alert(this.state.name + ":" + this.state.message);
         this.setState({
-            name: ''
-        })
+            name: '',
+            message: ''
+        });
     }
+
 
     render() {
         return (
@@ -27,9 +32,16 @@ class EventPractice extends Component {
                 <input
                     type='text'
                     name='message'
+                    placeholder='메시지를 입력하세요'
+                    value={this.state.message}
+                    onChange={this.handleChange} />
+
+                <input type='text'
+                    name='name'
                     placeholder='이름을 입력하세요'
                     value={this.state.name}
                     onChange={this.handleChange} />
+
                 <button onClick={this.handleClick}>확인</button>
             </>
         );
