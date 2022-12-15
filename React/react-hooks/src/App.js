1af75788009a6ca30c29d2f86f7f9177742b8758
kkkwp <chxrryda@gmail.com>
2022-12-15 16:44:23 +0900

    feat: 데이터 추가 기능 구현

--- React/react-hooks/src/App.js
+++ React/react-hooks/src/App.js
@@ -1,14 +1,62 @@
 import React from 'react';
+import { useState, useRef } from 'react';
+
 import UserList from './UserList';
+import CreateUser from './CreateUser';
+
 
 const App = () => {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    // state를 수정할 때는 원본을 복제한 후 수정
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  // 배열의 데이터를 수정하면 컴포넌트가 리랜더링 될 수 있도록 state로 생성
+  const [users, setUsers] = useState([
     { id: 1, username: 'karina', email: 'karina@gmail.com' },
     { id: 2, username: 'winter', email: 'winter@naver.com' }
-  ]
+  ]);
+
+  // 변수 생성
+  const nextId = useRef(3);
+
+  // 데이터 삽입
+  const onCreate = () => {
+    // 하나의 객체 생성
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+    // users에 user를 추가
+    setUsers([...users, user]);
+
+    // 입력 요소 초기화
+    setInputs({
+      username: '',
+      email: ''
+    })
+
+    // 다음 id를 위해서 id를 1 증가
+    nextId.current += 1;
+  }
 
   return (
     <div>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate} />
       <UserList users={users} />
     </div>
   )
