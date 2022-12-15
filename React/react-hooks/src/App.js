39dffec01901bea7438e42130054b25e4f1ce94a
kkkwp <chxrryda@gmail.com>
2022-12-15 16:39:40 +0900

    feat: 객체 배열을 함수형 컴포넌트로 출력하는 기능 구현

--- React/react-hooks/src/App.js
+++ React/react-hooks/src/App.js
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from 'react';
-
-const ClassEffect = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log("마운트와 업데이트가 끝나면 호출");
-    document.title = `You clicked ${count} times`;
-  });
-
-  return (
-    <div>
-      <p>클릭을 {this.state.count}번 수행</p>
-      <button onClick={(e) =>
-        setCount(count + 1)
-      }>+1</button>
-    </div>
-  );
-}
+import React from 'react';
+import UserList from './UserList';
 
 const App = () => {
+  const users = [
+    { id: 1, username: 'karina', email: 'karina@gmail.com' },
+    { id: 2, username: 'winter', email: 'winter@naver.com' }
+  ]
+
   return (
     <div>
-      <ClassEffect />
+      <UserList users={users} />
     </div>
   )
 }
