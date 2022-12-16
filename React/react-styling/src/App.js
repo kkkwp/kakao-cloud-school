689deb41cf6379d59d13880fccd77701f813ad9a
kkkwp <chxrryda@gmail.com>
2022-12-16 16:33:05 +0900

    feat:(data): Fetch API로 데이터 받아오는 기능 구현

--- React/react-styling/src/App.js
+++ React/react-styling/src/App.js
@@ -4,20 +4,10 @@ function App() {
     return (
         <div>
             <button onClick={(e) => {
-                let request = new XMLHttpRequest();
-                // GET 방식으로 요청
-                request.open('GET', 'https://jsonplaceholder.typicode.com/use');
-                // POST 방식일 때는 send에 파라미터를 대입
-                request.send('');
-                request.addEventListener('load', () => {
-                    let data = JSON.parse(request.responseText);
-                    // 데이터를 가져오는 데 성공했을 때
-                    console.log(data);
-                });
-                request.addEventListener('error', (error) => {
-                    // 데이터를 가져오는 데 실패했을 때
-                    console.log(error);
-                });
+                fetch('https://jsonplaceholder.typicode.com/users')
+                    .then((response) => response.json())
+                    .then((data) => console.log(data))
+                    .catch((error) => console.log(error.message))
             }}> 다운로드 </button>
         </div>
     );
