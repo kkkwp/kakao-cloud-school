15609d17b48b69101fea2a2c02377a7e08d120a0
kkkwp <chxrryda@gmail.com>
2022-12-16 16:31:41 +0900

    feat(data): ajax로 서버에서 데이터 받아오는 기능 구현

--- React/react-styling/src/App.js
+++ React/react-styling/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-import StyledComponent from "./components/StyledComponent";
-
 function App() {
     return (
         <div>
-            <StyledComponent>버튼</StyledComponent>
+            <button onClick={(e) => {
+                let request = new XMLHttpRequest();
+                // GET 방식으로 요청
+                request.open('GET', 'https://jsonplaceholder.typicode.com/use');
+                // POST 방식일 때는 send에 파라미터를 대입
+                request.send('');
+                request.addEventListener('load', () => {
+                    let data = JSON.parse(request.responseText);
+                    // 데이터를 가져오는 데 성공했을 때
+                    console.log(data);
+                });
+                request.addEventListener('error', (error) => {
+                    // 데이터를 가져오는 데 실패했을 때
+                    console.log(error);
+                });
+            }}> 다운로드 </button>
         </div>
     );
 }
