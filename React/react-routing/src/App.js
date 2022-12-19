43bac8f759178b3013d8020ebdf63b7aa7ce1ec7
kkkwp <chxrryda@gmail.com>
2022-12-19 16:27:00 +0900

    feat: 공통 레이아웃 적용

--- React/react-routing/src/App.js
+++ React/react-routing/src/App.js
@@ -5,15 +5,19 @@ import Profile from './Profile';
 
 import Article from './Article';
 import Articles from './Articles';
+import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/profile/:username' element={<Profile />} />
-      <Route path='/articles' element={<Articles />} />
-      <Route path='/articles/:id' element={<Article />} />
+      <Route element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile/:username' element={<Profile />} />
+      </Route>
+      <Route path='/articles' element={<Articles />}>
+        <Route path=':id' element={<Article />} />
+      </Route>
     </Routes>
   );
 }
