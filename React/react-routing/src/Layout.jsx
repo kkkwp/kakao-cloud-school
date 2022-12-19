4a9526e64db85e300993e047238aeb25133559a2
kkkwp <chxrryda@gmail.com>
2022-12-19 16:28:45 +0900

    feat: useNavigate 실습

--- React/react-routing/src/Layout.jsx
+++ React/react-routing/src/Layout.jsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom"
 
 const Layout = () => {
+    const navigate = useNavigate();
+
+    // 뒤로 이동하는 함수
+    const goBack = () => {
+        navigate(-1);
+    }
+
+    // articles로 이동
+    const goArticles = () => {
+        navigate("/articles");
+    }
+
     return (
         <div>
             <header style={{
                 background: 'lightgray',
                 padding: 16, fontSize: 24
             }}>
-                Header
+                <button onClick={goBack}>뒤로</button>
+                <button onClick={goArticles}>게시글</button>
             </header>
             <main>
                 <Outlet />
