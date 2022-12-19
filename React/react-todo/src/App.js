205338438e370ebac026c3a06073d8904bcb053f
kkkwp <chxrryda@gmail.com>
2022-12-19 14:18:04 +0900

    feat: 많은 양의 데이터 랜더링

--- React/react-todo/src/App.js
+++ React/react-todo/src/App.js
@@ -6,32 +6,26 @@ import ToDoList from "./components/ToDoList";
 // useCallback은 함수를 효율적으로 생성하기 위해서
 import { useState, useRef, useCallback } from 'react';
 
-function App() {
-  const [todos, setToDos] = useState([
-    {
-      id: 1,
-      text: 'HTML, CSS, JavaScript',
-      checked: true
-    },
-    {
-      id: 2,
-      text: 'Node.js',
-      checked: true
-    },
-    {
-      id: 3,
-      text: 'MariaDB, MongoDB',
-      checked: true
-    },
-    {
-      id: 4,
-      text: 'React',
+// 대량의 데이터를 생성해서 리턴하는 함수
+const createBulkTodos = () => {
+  const array = [];
+  for (let i = 1; i <= 2000; i++) {
+    array.push({
+      id: i,
+      text: `${i}번째 할 일 `,
       checked: false
-    },
-  ]);
+    })
+  }
+  return array;
+}
+
+function App() {
+  // useState에 데이터를 생성하는 함수를 대입할 때, 함수 호출 구문을 대입하면 데이터가 만들어질 때마다 리랜더링
+  // 함수 이름을 대입해야 함수를 전부 수행하고 한 번만 리랜더링을 수행
+  const [todos, setToDos] = useState(createBulkTodos);
 
   // 아이디를 위한 변수 생성
-  const nextId = useRef(5);
+  const nextId = useRef(2001);
 
   // 삽입을 처리하기 위한 함수
   // todos에 변화가 생기면 함수를 만들지만, 그렇지 않으면 기존 함수를 이용
