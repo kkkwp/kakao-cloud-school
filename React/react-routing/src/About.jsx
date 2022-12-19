ea6efd435b3d5cb5227fc3d432293c7e29a54bfb
kkkwp <chxrryda@gmail.com>
2022-12-19 16:19:46 +0900

    feat: query string 읽어오기 실습

--- React/react-routing/src/About.jsx
+++ React/react-routing/src/About.jsx
@@ -1,7 +1,13 @@
+import { useLocation } from 'react-router-dom';
+
 const About = () => {
+    // query string을 읽을 수 있는 Hook
+    const location = useLocation();
+
     return (
         <div>
             <h1>React Router 실습</h1>
+            <p>Query String: {location.search}</p>
         </div>
     )
 }
