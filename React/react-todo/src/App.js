3a42686fd2c139e4e2580a9cff07ff92d1e1ba8b
kkkwp <chxrryda@gmail.com>
2022-12-19 12:47:38 +0900

    feat: 데이터 삽입 기능 구현

--- React/react-todo/src/App.js
+++ React/react-todo/src/App.js
@@ -2,7 +2,9 @@ import ToDoTemplate from "./components/ToDoTemplate";
 import ToDoInsert from "./components/ToDoInsert";
 import ToDoList from "./components/ToDoList";
 
-import { useState } from 'react';
+// useRef는 변수를 생성하거나 변수를 만들어 DOM에 할당하기 위해서
+// useCallback은 함수를 효율적으로 생성하기 위해서
+import { useState, useRef, useCallback } from 'react';
 
 function App() {
   const [todos, setToDos] = useState([
@@ -28,10 +30,24 @@ function App() {
     },
   ]);
 
+  // 아이디를 위한 변수 생성
+  const nextId = useRef(5);
+
+  // 삽입을 처리하기 위한 함수
+  // todos에 변화가 생기면 함수를 만들지만, 그렇지 않으면 기존 함수를 이용
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false
+    }
+    setToDos(todos.concat(todo));
+    nextId.current += 1;
+  }, [todos]);
 
   return (
     <ToDoTemplate>
-      <ToDoInsert />
+      <ToDoInsert onInsert={onInsert} />
       <ToDoList todos={todos} />
     </ToDoTemplate>
   );
