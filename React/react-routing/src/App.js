badfb9b69c187d5412340eb452001fbc1aedfb57
kkkwp <chxrryda@gmail.com>
2022-12-19 16:32:21 +0900

    feat: Navigate 실습

--- React/react-routing/src/App.js
+++ React/react-routing/src/App.js
@@ -7,11 +7,17 @@ import Article from './Article';
 import Articles from './Articles';
 import Layout from './Layout';
 
+import Login from './Login';
+import MyPage from './MyPage';
+
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+
       <Route element={<Layout />}>
-        <Route path='/' element={<Home />} />
+        <Route index element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile/:username' element={<Profile />} />
       </Route>
