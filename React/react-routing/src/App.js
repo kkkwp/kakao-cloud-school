be7efc3d2b3a6c9eb6b79dce0f2cb24dffa4cbb3
kkkwp <chxrryda@gmail.com>
2022-12-19 16:17:26 +0900

    feat: query string

--- React/react-routing/src/App.js
+++ React/react-routing/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
+      <Route path='/profile/:username' element={<Profile />} />
     </Routes>
   );
 }
--- React/react-routing/src/Home.jsx
+++ React/react-routing/src/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
     return (
         <div>
             <h1>메인 페이지</h1>
-            <Link to='/about'>소개</Link>
+            <ul>
+                <li><Link to='/about'>소개</Link></li>
+                <li><Link to='/profile/karina'>카리나</Link></li>
+                <li><Link to='/profile/winter'>윈터</Link></li>
+                <li><Link to='/profile/void'>모르는 유저</Link></li>
+            </ul>
         </div>
     )
 }
