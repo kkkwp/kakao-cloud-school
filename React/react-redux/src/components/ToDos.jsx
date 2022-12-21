a76aa4cad447b153dd4f2cd81881247c34cef29a
kkkwp <chxrryda@gmail.com>
2022-12-21 10:46:53 +0900

    feat(redux): todos 수정

--- React/react-redux/src/components/ToDos.jsx
+++ React/react-redux/src/components/ToDos.jsx
@@ -4,31 +4,45 @@ import React from 'react';
 const ToDoItem = ({ todo, onToggle, onRemove }) => {
     return (
         <div>
-            <input type='checkbox' />
-            <span>텍스트</span>
-            <button>삭제</button>
+            <input
+                type='checkbox'
+                onClick={() => onToggle(todo.id)}
+                checked={todo.done}
+                readOnly={true} />
+            <span
+                style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+                {todo.text}
+            </span>
+            <button onClick={() => onRemove(todo.id)}>삭제</button>
         </div>
     );
 }
 
 // 여러 개의 ToDoItem을 출력할 컴포넌트
-const ToDos = ({ input, todos, onChangeInput, onInsert, onToggel, onRemove }) => {
+const ToDos = ({ input, todos, onChangeInput, onInsert, onToggle, onRemove }) => {
     const onSubmit = (e) => {
         e.preventDefault();
+        onInsert(input);
+        onChangeInput('');
     }
+
+    const onChange = (e) =>
+        onChangeInput(e.target.value);
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input />
+                <input value={input} onChange={onChange} />
                 <button type="submit">등록</button>
             </form>
             <div>
-                <ToDoItem />
-                <ToDoItem />
-                <ToDoItem />
-                <ToDoItem />
-                <ToDoItem />
+                {todos.map(todo => (
+                    <ToDoItem
+                        todo={todo}
+                        key={todo.id}
+                        onToggle={onToggle}
+                        onRemove={onRemove} />
+                ))}
             </div>
         </div>
     )
