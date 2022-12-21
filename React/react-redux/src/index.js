b902d6cba7058dd04b78cf67959ab558565b2dc4
kkkwp <chxrryda@gmail.com>
2022-12-21 10:20:45 +0900

    feat: Redux Module 실습

--- React/react-redux/src/index.js
+++ React/react-redux/src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "./exercise";
+import rootReducer from './modules';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
+//console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
