6fd54d27af68b7db57d07f58f63498d24252a715
kkkwp <chxrryda@gmail.com>
2022-12-21 12:34:14 +0900

    feat(component): ref 실습

--- React/react-component/src/App.js
+++ React/react-component/src/App.js
@@ -4,16 +4,21 @@ import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 
-function App() {
-  return (
-    <>
-      <MyComponent name={'star'} year={2022}>태그 안의 내용</MyComponent>
-      <StateComponent />
-      <EventPractice />
-      <ValidationSample />
-      <ScrollBox />
-    </>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <MyComponent name={'star'} year={2022}>태그 안의 내용</MyComponent>
+        <StateComponent />
+        <EventPractice />
+        <ValidationSample />
+        <ScrollBox ref={(ref) => this.scrollbox = ref} />
+        <button onClick={(e) => { this.scrollbox.scrollToBottom() }}>맨 아래로</button>
+      </>
+    );
+  }
 }
 
 export default App;
