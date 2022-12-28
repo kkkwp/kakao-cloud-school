cb5e293f9b8a401678e5a780b918ac766a0521ca
kkkwp <chxrryda@gmail.com>
2022-12-28 11:19:13 +0900

    feat: 데이터 공유 실습

---
+++ Java/practice/src/kakao/itstudy/Data/GlobalData.java
@@ -0,0 +1,6 @@
+package kakao.itstudy.Data;
+
+// 이 경우 다른 모든 클래스에서 GlobalData.global의 형태로 공유 가능
+public class GlobalData {
+    public static int global;
+}
---
+++ Java/practice/src/kakao/itstudy/Data/GlobalDataMain.java
@@ -0,0 +1,9 @@
+package kakao.itstudy.Data;
+
+public class GlobalDataMain {
+
+    public static void main(String[] args) {
+        // public 클래스에서 public static 속성은 어느 곳에서나 호출 가능
+        GlobalData.global = 10;
+    }
+}
