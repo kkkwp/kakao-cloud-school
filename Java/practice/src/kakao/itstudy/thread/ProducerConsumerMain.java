b32a6c5249f028d1169553e0c8b83a3720aa16e1
kkkwp <chxrryda@gmail.com>
2022-12-28 12:40:13 +0900

    feat: 동기화를 통해 문제 해결

--- Java/practice/src/kakao/itstudy/thread/ProducerConsumerMain.java
+++ Java/practice/src/kakao/itstudy/thread/ProducerConsumerMain.java
@@ -3,29 +3,35 @@ package kakao.itstudy.thread;
 import java.util.ArrayList;
 import java.util.List;
 
-// 공유 자원 클래스
 class Product {
     // 공유 자원
     List<Character> list;
 
-    // 공유 자원을 넘겨받기 위한 생성자
     public Product(List<Character> list) {
         this.list = list;
     }
 
     // 공유 자원에 데이터를 삽입하는 메서드
-    public void put(char ch) {
+    synchronized void put(char ch) {
         list.add(ch);
         System.out.println(ch + "를 입고");
         try {
             Thread.sleep(1000);
         } catch (Exception e) {}
         System.out.println("재고 수량: " + list.size());
+        // 대기 중인 스레드가 작업을 수행하도록 신호를 전송
+        notify();
     }
 
     // 공유 자원을 소비하는 메서드
-    public void get() {
-        //첫번째 데이터 꺼내기
+    synchronized void get() {
+        if (list.size() == 0) {
+            try {
+                // notify가 호출될 때까지 대기
+                wait();
+            } catch (Exception e) {}
+        }
+        // 첫번째 데이터 꺼내기
         Character ch = list.remove(0);
         System.out.println(ch + "를 출고");
         try {
@@ -33,7 +39,6 @@ class Product {
         } catch (Exception e) {}
         System.out.println("재고 수량: " + list.size());
     }
-
 }
 
 // 생산자 스레드
@@ -62,7 +67,7 @@ class Customer extends Thread {
 
     // 스레드로 수행할 내용
     public void run() {
-        for (int i = 0; i < 26; i++) {
+        for (int i=0; i<26;i++) {
             product.get();
         }
     }
