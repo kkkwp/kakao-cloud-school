0186549eb970c89eeb0583e4ae11ee0a8359817a
kkkwp <chxrryda@gmail.com>
2023-01-03 10:48:58 +0900

    feat: 파일에 바이트 단위로 기록하기

--- Java/practice/src/kakao/itstudy/io/ByteStreamMain.java
+++ Java/practice/src/kakao/itstudy/io/ByteStreamMain.java
@@ -1,11 +1,22 @@
 package kakao.itstudy.io;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Arrays;
 
 public class ByteStreamMain {
 
     public static void main(String[] args) {
+        // 파일에 바이트 단위로 기록
+        try (FileOutputStream fos = new FileOutputStream("./sample.txt", true)) {
+            // 파일에 기록할 내용 만들기
+            String msg = " Hello Stream";
+            fos.write(msg.getBytes());
+            fos.flush();
+        } catch (Exception e) {
+            java.lang.System.out.println(e.getLocalizedMessage());
+        }
+
         // 파일을 바이트 단위로 읽어오기
         try (FileInputStream fis = new FileInputStream("./sample.txt")) {
             while (true) {
@@ -25,9 +36,9 @@ public class ByteStreamMain {
                     break;
                 } else {
                     // 숫자 배열 출력 - 텍스트가 아닌 경우
-                    System.out.println(Arrays.toString(b)); // [115, 97, 109, 112, 108, 101]
+                    System.out.println(Arrays.toString(b)); // [115, 97, 109, 112, 108, 101, 32, 72, 101, 108, 108, 111, 32, 83, 116, 114, 101, 97, 109]
                     // 문자열로 변환해서 출력 - 텍스트인 경우
-                    System.out.println(new String(b)); // sample
+                    System.out.println(new String(b)); // sample Hello Stream
                 }
             }
         } catch (Exception e) {
