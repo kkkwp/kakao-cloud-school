cded3a42e4a1cb9c4fba55338f611d5d7a17d575
kkkwp <chxrryda@gmail.com>
2023-01-05 15:02:14 +0900

    feat: thread를 활용하여 url 통신으로 문자열 다운로드 실습

---
+++ Java/practice/src/kakao/itstudy/network/AsyncStringDownload.java
@@ -0,0 +1,43 @@
+package kakao.itstudy.network;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class AsyncStringDownload {
+
+    public static void main(String[] args) {
+        // 스레드를 만들어서 다운로드 - 비동기 방식
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    // url을 생성
+                    URL url = new URL("https://www.kakao.com");
+                    // 연결 객체 생성
+                    HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+                    // 연결 옵션 설정
+                    con.setConnectTimeout(10000);
+                    con.setUseCaches(false);
+                    con.setRequestMethod("GET");
+
+                    // 읽기 위한 스트림 생성
+                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    while (true) {
+                        String line = br.readLine();
+                        if (line == null) break;
+                        sb.append(line + "\n");
+                    }
+                    String html = sb.toString();
+                    System.out.println(html);
+                    br.close();
+                    con.disconnect();
+                } catch (Exception e) {
+                    System.out.println(e.getLocalizedMessage());
+                }
+            }
+        }.start();
+    }
+}
