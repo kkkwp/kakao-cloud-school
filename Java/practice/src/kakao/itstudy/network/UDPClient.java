5f2d29222f432c75503347b18b7db4ed68af472a
kkkwp <chxrryda@gmail.com>
2023-01-05 14:33:48 +0900

    feat: UDP 통신 실습

---
+++ Java/practice/src/kakao/itstudy/network/UDPClient.java
@@ -0,0 +1,30 @@
+package kakao.itstudy.network;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class UDPClient {
+
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                System.out.print("보낼 메시지: ");
+                String msg = sc.nextLine();
+                // 보낼 곳의 IP 생성
+                InetAddress addr = InetAddress.getByName("192.168.0.152");
+                // 소켓 생성
+                DatagramSocket ds = new DatagramSocket();
+                // 보낼 패킷 생성
+                DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, addr, 7777);
+                ds.send(dp);
+                ds.close();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+}
---
+++ Java/practice/src/kakao/itstudy/network/UDPServer.java
@@ -0,0 +1,26 @@
+package kakao.itstudy.network;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPServer {
+
+    public static void main(String[] args) {
+        // UDP 소켓 생성
+        try (DatagramSocket dsoc = new DatagramSocket(7777)) {
+            byte[] data = new byte[65536];
+            // 데이터를 전송받을 패킷 클래스의 인스턴스 생성
+            DatagramPacket dp = new DatagramPacket(data, data.length);
+            while (true) {
+                // 데이터 받기
+                dsoc.receive(dp);
+                // 클라이언트 정보 확인
+                System.out.println(dp.getAddress());
+                // 받은 메시지 출력
+                System.out.println(new String(dp.getData()));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+}
