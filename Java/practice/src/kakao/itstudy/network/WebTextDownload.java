a755c82abd97d1768ec79ef17020dc1fac132f58
kkkwp <chxrryda@gmail.com>
2023-01-05 14:18:42 +0900

    feat: TCP를 이용해서 web 문자열 다운로드 실습

---
+++ Java/practice/src/kakao/itstudy/network/WebTextDownload.java
@@ -0,0 +1,41 @@
+package kakao.itstudy.network;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class WebTextDownload {
+
+    public static void main(String[] args) {
+        try {
+            // kakao.com의 주소 정보 가져오기
+            InetAddress ia = InetAddress.getByName("www.kakao.com");
+            // 카카오에 연결 - 소켓 생성
+            Socket socket = new Socket(ia, 80);
+            // 요청을 전송할 수 있는 스트림을 생성
+            PrintWriter ps = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+            // 요청 전송
+            ps.println("GET https://www.kakao.com");
+            ps.flush();
+
+            // 문자 단위로 전송을 받기 위한 스트림 생성
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // 여러 줄의 문자열을 하나로 만들기
+            StringBuilder sb = new StringBuilder();
+            while (true) {
+                String imsi = br.readLine();
+                if (imsi == null) break;
+                sb.append(imsi + "\n");
+            }
+            String html = sb.toString();
+            System.out.println(html);
+            // 사용한 자원 정리
+            br.close();
+            ps.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+}
